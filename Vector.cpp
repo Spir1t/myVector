ba7d90b2026f6ebb8ec46dccdfe4d3087d4dd47f
Spir1t <rinchinov.slava@mail.ru>
2023-06-02 20:54:35 +0300

    everything done

--- Vector.cpp
+++ Vector.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 #include "Vector.h"
 
 
@@ -6,66 +7,61 @@ Vector::Vector(const Value* rawArray, const size_t size, float coef)
 :_size(size)
 , _multiplicativeCoef(coef)
 , _capacity(size * coef)
-,_data(new Value[_capacity])
 {
-    memcpy(&_data[0], &rawArray[0], size);
-    /*
-    _size = size;
-    _multiplicativeCoef = coef;
-    _capacity = size * coef;
     _data = new Value[_capacity];
-    */
-    /*
-    for(int i = 0; i < size; i++)
-    {
-            _data[i] = rawArray[i];
-    }
-    */
+    memcpy(&_data[0], &rawArray[0], size * sizeof(Value));
 }
 
 Vector::Vector(const Vector& other)
 : _size(other._size)
 , _multiplicativeCoef(other._multiplicativeCoef)
-, _capacity(other._size * _multiplicativeCoef)
-, _data(new Value[_capacity])
+, _capacity(other._size * other._multiplicativeCoef)
 {
-    memcpy(&_data[0], &other._data[0], _size);
+    _data = new Value[_capacity];
+    memcpy(_data, other._data, _size * sizeof(Value));
 }
 
 Vector& Vector::operator=(const Vector& other)
 {
-    delete[] _data;
+    if (this == &other)
+    {
+        return(*this);
+    }
     _size = other._size;
     _multiplicativeCoef = other._multiplicativeCoef;
-    _capacity = other._size * _multiplicativeCoef;
+    _capacity = other._size;
+    delete[] _data;
     _data = new Value[_capacity];
-    memcpy(&_data[0], &other._data[0], _size);
+    memcpy(_data, other._data, _size * sizeof(Value));
     return(*this);
 }
 
 Vector::Vector(Vector&& other) noexcept
-: _size(other._size)
-, _multiplicativeCoef(other._multiplicativeCoef)
-, _capacity(other._size * _multiplicativeCoef)
 {
-    _data = new Value[_capacity];
-    //for(int i = 0; i < _size; i++)
-    //{
-      //  _data[i] = other._data[i];
-    //}
     _data = other._data;
-    delete[] other._data;
+    _multiplicativeCoef = other._multiplicativeCoef;
+    _capacity = other._capacity;
+    _size = other._size;
+
+    other._data = nullptr;
+    other._capacity = 0;
+    other._size = 0;
 }
 
 Vector& Vector::operator=(Vector&& other) noexcept
 {
-    delete[] _data;
+    if(this == &other)
+    {
+        return(*this);
+    }
     _size = other._size;
     _multiplicativeCoef = other._multiplicativeCoef;
-    _capacity = other._size * _multiplicativeCoef;
-    _data = new Value[_capacity];
+    _capacity = other._capacity;
+    delete[] _data;
+
     _data = other._data;
-    delete[] other._data;
+    other._data = nullptr;
+    other._capacity = 0;
     return(*this);
 }
 
@@ -74,79 +70,73 @@ Vector::~Vector()
     delete[] _data;
 }
 
-void Vector::pushBack(const Value& value)
-{
-    _size += 1;
-    if(_size < _capacity)
+void Vector::pushBack(const Value &value) {
+    if(_capacity == 0)
     {
-        _data[_size - 1] = value;
+        reserve(_multiplicativeCoef);
     }
-    else
+
+    else if(_size == _capacity) 
     {
-        _capacity *= _multiplicativeCoef;
-        Value* newData = new Value[_capacity];
-        memcpy(&newData[0], &_data[0], _size - 1);
-        newData[_size - 1] = value;
-        delete[] _data;
-        _data = newData;
+        reserve(_capacity * _multiplicativeCoef);
     }
+
+    _data[_size] = value;
+    _size++;
 }
 
 void Vector::pushFront(const Value& value)
 {
-    _size += 1;
-    if(_size < _capacity)
+    if(_capacity == 0)
     {
-        _data[_size - 1] = value;
+        reserve(_multiplicativeCoef);
     }
-    else
+    
+    else if(_size == _capacity)
     {
-        _capacity *= _multiplicativeCoef;
-        Value* newData = new Value[_capacity];
-        newData[0] = value;
-        memcpy(&newData[1], &_data[0], _size - 1);
-        delete[] _data;
-        _data = newData;
+        reserve(_capacity * _multiplicativeCoef);
     }
+
+    memmove(&_data[1], _data, _size * sizeof(Value));
+    _data[0] = value;
+    _size++;
 }
 
 void Vector::insert(const Value& value, size_t pos)
 {
+    if(pos > _size)
+    {
+        throw std::out_of_range("Incorrect pos");
+    }
+    
+    if(_size == _capacity)
+    {
+        reserve(_capacity * _multiplicativeCoef);
+    }
+    memmove(&_data[pos + 1], &_data[pos], (_size - pos) * sizeof(Value));
+    _data[pos] = value;
     _size++;
-    if(_size < _capacity)
-    {
-        memcpy(&_data[pos + 1], &_data[pos], sizeof(Value) * (_size - 1 - pos));
-        _data[pos] = value;
-    }
-    else
-    {
-        _capacity *= _multiplicativeCoef;
-        Value* newData = new Value[_capacity];
-        memcpy(&newData[0], &_data[0], sizeof(Value) * pos);
-        newData[pos] = value;
-        memcpy(&newData[pos + 1], &_data[pos], sizeof(Value) * (_size - 1 - pos));
-        delete[] _data;
-        _data = newData;
-    }
 }
+
 void Vector::insert(const Value* values, size_t size, size_t pos)
 {
+    if(pos > _size)
+    {
+        throw std::out_of_range("incorrect pos");
+    }
+
+    if(values == nullptr)
+    {
+        throw std::out_of_range("Incorrect values");
+    }
+
+    if(_size + size >= _capacity)
+    {
+        reserve((_size + size) * _multiplicativeCoef);
+    }
+    memmove(&_data[pos + size], &_data[pos], sizeof(Value) * (_size - pos));
+    memcpy(&_data[pos], values, sizeof(Value) * size);
     _size += size;
-    if(_size < _capacity)
-    {
-        memcpy(&_data[pos + size], &_data[pos], sizeof(Value) * (_size - 1 - pos));
-        memcpy(&_data[pos], &values[0], sizeof(Value) * (size));
-    }
-    else
-    {
-        _capacity *= _multiplicativeCoef;
-        Value* newData = new Value[_capacity];
-        memcpy(&newData[0], &_data[0], sizeof(Value) * pos);
-        memcpy(&newData[pos], &values[0], sizeof(Value) * size);
-        memcpy(&newData[pos + size], &_data[pos], sizeof(Value) * (size - 1 - pos));
-        delete[] _data;
-        _data = newData;
-    }
 }
 
 void Vector::insert(const Vector& vector, size_t pos)
@@ -156,30 +146,50 @@ void Vector::insert(const Vector& vector, size_t pos)
 
 void Vector::popBack()
 {
-    _size--;
-    delete &_data[_size - 1];
+    if (_size == 0)
+    {
+        throw(std::out_of_range("Nothing to pop"));
+    }
+    else
+    {
+        _size--;
+    }
 }
 
 void Vector::popFront()
 {
-    _size--;
-    delete &_data[0];
-    memmove(&_data[0], &_data[1], sizeof(Value) * _size);
+    if (_size == 0)
+    {
+        throw(std::out_of_range("Nothing to pop"));
+    }
+    else
+    {
+        _size--;
+        memmove(&_data[0], &_data[1], sizeof(Value) * _size);
+    }
 }
 
 void Vector::erase(size_t pos, size_t count)
 {
-    for(int i = 0; i < count; i++)
+    if(pos + count > _size)
     {
-        delete &_data[pos + i];
+        count = _size - pos;
     }
-    memmove(&_data[pos], &_data[pos + count], sizeof(int) * (_size - pos - count));
+    if(pos < 0 || count < 0)
+    {
+        return;
+    }
+    memmove(&_data[pos], &_data[pos + count], sizeof(Value) * (_size - pos - count));
     _size -= count;
 }
 
 void Vector::eraseBetween(size_t beginPos, size_t endPos)
 {
-    int count = endPos - beginPos + 1;
+    if(beginPos < endPos)
+    {
+        return;
+    }
+    int count = endPos - beginPos;
     erase(beginPos, count);
 }
 
@@ -195,7 +205,7 @@ size_t Vector::capacity() const
 
 double Vector::loadFactor() const
 {
-    return _multiplicativeCoef;
+    return ((double) _size /  _capacity);
 }
 
 Value& Vector::operator[](size_t idx)
@@ -226,20 +236,29 @@ void Vector::reserve(size_t capacity)
     if(capacity > _capacity)
     {
         Value* newData = new Value[capacity];
-        memcpy(&newData[0], &_data[0], _size);
-        delete[] _data;
-        _data = newData;
+        for (int i = 0; i < _size; i++) {
+            newData[i] = _data[i];
+        }
+        delete _data;
+
         _capacity = capacity;
+        _data = newData;
     }
 }
 
 void Vector::shrinkToFit()
 {
-    for(int i = _size; i < _capacity; i++)
+    if(_size < _capacity)
     {
-        delete &_data[i];
+            Value* newData = new Value[_size];
+            for(int i = 0; i < _size; i++)
+            {
+                newData[i] = _data[i];
+            }
+            delete[] _data;
+            _data = newData;
+            _capacity = _size;
     }
-    _capacity = _size;
 }
 
 Vector::Iterator::Iterator(Value* ptr): _ptr(ptr) {}
@@ -262,4 +281,4 @@ bool Vector::Iterator::operator!=(const Iterator& other) const {return((_ptr != 
 
 Vector::Iterator Vector::begin() {return Iterator(&_data[0]);}
 
-Vector::Iterator Vector::end() {return Iterator(&_data[_size]);}
+Vector::Iterator Vector::end() {return Iterator(&_data[_size]);}git
--- test.cpp
+++ test.cpp
@@ -3,7 +3,21 @@ using namespace std;
 
 int main()
 {
-    int a(6);
-    a = 5;
-    return 0;
+    int _size = 5;
+    int _capacity = 7;
+    int* _data = new int[_capacity];
+    for(int i = 0; i < _size; i++) {_data[i] = i;}
+    for(int i = 0; i < _size; i++) {cout << _data[i] << " ";}
+    _size++;
+    int pos = 3;
+    cout << endl;
+
+    _capacity *= 2;
+    int* newData = new int[_capacity];
+    memcpy(&newData[0], &_data[0], sizeof(int) * pos);
+    newData[pos] = 10;
+    memcpy(&newData[pos + 1], &_data[pos], sizeof(int) * (_size - 1 - pos));
+    for(int i = 0; i < _size; i++) {cout << newData[i] << " ";}
+    delete _data;
+    delete newData;
 }
